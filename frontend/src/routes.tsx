03ad1da9fadfe48b0d537a9e10e6ef34f8d987f3
Hasan Safdari <41860385+tahasafdari@users.noreply.github.com>
2023-09-29 17:38:14 +0300

    Add more template routes to the route

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -6,7 +6,35 @@ const routes: IRoute[] = [
         name : 'Home',
         path : '/dashboard',
         icon : HomeIcon,
-    } 
+    } ,
+    {
+        name : 'Item1',
+        path : '/dashboard',
+        icon : HomeIcon,
+    } ,
+    {
+        name : 'Item2',
+        path : '/dashboard',
+        icon : HomeIcon,
+    } ,
+    {
+        name : 'Item3',
+        path : '/dashboard',
+        icon : HomeIcon,
+    } ,
+    {
+        name : 'Item4',
+        path : '/dashboard',
+        icon : HomeIcon,
+    } ,
+    {
+        name : 'Item5',
+        path : '/dashboard',
+        icon : HomeIcon,
+    } ,
+
+
+
 
 ]
 
