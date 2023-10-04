4b5443bd7082528d435811d83f2d8ea3024d1490
Sampo <sampoo@metropolia.fi>
2023-10-04 17:31:44 +0300

    added modules and converters to query games from rawg

--- Server/src/api/resolvers/gameResolver.ts
+++ Server/src/api/resolvers/gameResolver.ts
@@ -1,5 +1,6 @@
 import fetchData from '../../functions/fetchData';
 import {Game} from '../../interfaces/Game';
+import {fetchByName} from '../../utils/api-fetcher';
 import gameModel from '../models/gameModel';
 import {GraphQLError} from 'graphql';
 
@@ -8,6 +9,14 @@ export default {
     games: async () => {
       const games = gameModel.find();
       return games;
+    },
+    externalGamesByName: async (_: undefined, args: {name: string}) => {
+      try {
+        const games: Game[] = await fetchByName(args.name);
+        return games;
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 
---
+++ Server/src/utils/RawgToGame.ts
@@ -0,0 +1,14 @@
+import { Game } from "../interfaces/Game";
+import { GameFromRawg } from "../interfaces/GameFromRawg";
+
+export default {
+    format: (rawgGame: GameFromRawg) => {
+        const game = {
+            title: rawgGame.name,
+            apiId: rawgGame.id,
+        };
+        // console.log(`Game in converter: ${game.title} - ${game.apiId}`);
+        
+        return game as Game;
+    }
+};
