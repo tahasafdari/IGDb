f107ca5f4ff4ded8a9b85142590b57c7fe155bef
Mohammed Al-Jewari <mjassim18@gmail.com>
2023-10-05 13:32:40 +0300

    Fixing the fetching of external games

--- Server/src/utils/api-fetcher.ts
+++ Server/src/utils/api-fetcher.ts
@@ -1,7 +1,6 @@
 import {Game} from '../interfaces/Game';
 import RawgToGame from './RawgToGame';
 
-
 const fetchById = async (id: string) => {
   return (await fetch(`https://api.rawg.io/api/games/${id}`, {
     method: 'GET',
@@ -14,19 +13,22 @@ const fetchById = async (id: string) => {
 };
 
 const fetchByName = async (name: string) => {
-  const res = await fetch(`https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}&search=${name}`, {
-    method: 'GET',
-    headers: {
+  const res = await fetch(
+    `https://api.rawg.io/api/games?key=${process.env.RAWG_API_KEY}&search=${name}`,
+    {
+      method: 'GET',
+      headers: {
         'X-Auth-Token': process.env.RAWG_API_KEY as string,
-    },
-  });
+      },
+    }
+  );
 
   const data = await res.json();
   const gameArr: Game[] = [data.count];
-
+  console.log(data.results[0]);
   console.log(data.results.length);
-  
-  for (let index = 0; index < data.results.length; index++) {    
+
+  for (let index = 0; index < data.results.length; index++) {
     gameArr[index] = RawgToGame.format(data.results[index]);
   }
   return gameArr;
