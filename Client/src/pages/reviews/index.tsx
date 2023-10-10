3750da538b6a0ae86857e5db62d511a718944d28
Hasan Safdari <41860385+tahasafdari@users.noreply.github.com>
2023-10-10 19:30:55 +0300

    add reviews page

--- Client/src/pages/reviews/index.tsx
+++ Client/src/pages/reviews/index.tsx
@@ -4,9 +4,10 @@ import React, { useState } from 'react';
 import GameDetails from '@/components/reviews/gameDetails/gamedetails';
 import ReviewInput from '@/components/reviews/reviewInput/reviewInput';
 import SubmitButton from '@/components/reviews/submitButton/submitButton';
-import UserReviews from '@/components/reviews/userReviewProps/index';
+import UserReviews from '@/components/reviews/userReviewProps/userReview';
 import Loading from '@/components/reviews/load/loading';
-import aoubtIGDB from '@/components/images/aboutIGDB.png'
+import aboutIGDB from '@/components/image/aboutIGDB.png'
+import avatar1 from '../../../public/assets/avatars/avatar1.png'
 
 function ReviewPage() {
   const [userReview, setUserReview] = useState('');
@@ -17,12 +18,19 @@ function ReviewPage() {
     title: "Awesome Game",
     releaseDate: "2023-10-08",
     genre: "Adventure",
-    coverImage: "url(https://source.unsplash.com/random?gaming)"
+    coverImage: 'https://media.discordapp.net/attachments/942001801799024643/1160620037208543444/image.png?ex=653552a4&is=6522dda4&hm=70796db29d951010a44b1eebffc35d53561e29b98bad24b59ec7ca122f5ed8fc&=&width=756&height=534',
   };
   
   const userReviews = [
-    { username: "John", date: "2023-10-07", review: "Loved it!", rating: 5 },
-    { username: "Jane", date: "2023-10-06", review: "Good game but has bugs.", rating: 3 }
+    { username: "John",profileImage: 'https://media.discordapp.net/attachments/942001801799024643/1160620037208543444/image.png?ex=653552a4&is=6522dda4&hm=70796db29d951010a44b1eebffc35d53561e29b98bad24b59ec7ca122f5ed8fc&=&width=756&height=534' ,  date: "2023-10-07", review: "Loved it!", rating: 5 },
+    { username: "John",profileImage:'https://media.discordapp.net/attachments/942001801799024643/1160620037208543444/image.png?ex=653552a4&is=6522dda4&hm=70796db29d951010a44b1eebffc35d53561e29b98bad24b59ec7ca122f5ed8fc&=&width=756&height=534' , date: "2023-10-07", review: "Loved it!", rating: 5 },
+    { username: "John",profileImage:'https://media.discordapp.net/attachments/942001801799024643/1160620037208543444/image.png?ex=653552a4&is=6522dda4&hm=70796db29d951010a44b1eebffc35d53561e29b98bad24b59ec7ca122f5ed8fc&=&width=756&height=534', date: "2023-10-07", review: "Loved it!", rating: 4.5 },
+    { username: "John",profileImage:'https://media.discordapp.net/attachments/942001801799024643/1160620037208543444/image.png?ex=653552a4&is=6522dda4&hm=70796db29d951010a44b1eebffc35d53561e29b98bad24b59ec7ca122f5ed8fc&=&width=756&height=534', date: "2023-10-07", review: "Loved it!", rating: 3 },
+    { username: "John",profileImage:'https://media.discordapp.net/attachments/942001801799024643/1160620037208543444/image.png?ex=653552a4&is=6522dda4&hm=70796db29d951010a44b1eebffc35d53561e29b98bad24b59ec7ca122f5ed8fc&=&width=756&height=534' ,date: "2023-10-07", review: "Loved it!", rating: 1 },
+    { username: "John",profileImage:'https://media.discordapp.net/attachments/942001801799024643/1160620037208543444/image.png?ex=653552a4&is=6522dda4&hm=70796db29d951010a44b1eebffc35d53561e29b98bad24b59ec7ca122f5ed8fc&=&width=756&height=534', date: "2023-10-07", review: "Loved it!", rating: 5 },
+    { username: "John",profileImage:'https://media.discordapp.net/attachments/942001801799024643/1160620037208543444/image.png?ex=653552a4&is=6522dda4&hm=70796db29d951010a44b1eebffc35d53561e29b98bad24b59ec7ca122f5ed8fc&=&width=756&height=534' ,date: "2023-10-07", review: "Loved it!", rating: 2 },
+    { username: "John",profileImage:'https://media.discordapp.net/attachments/942001801799024643/1160620037208543444/image.png?ex=653552a4&is=6522dda4&hm=70796db29d951010a44b1eebffc35d53561e29b98bad24b59ec7ca122f5ed8fc&=&width=756&height=534' ,date: "2023-10-07", review: "Loved it!", rating: 4 },
+    { username: "Jane",profileImage:'https://media.discordapp.net/attachments/942001801799024643/1160620037208543444/image.png?ex=653552a4&is=6522dda4&hm=70796db29d951010a44b1eebffc35d53561e29b98bad24b59ec7ca122f5ed8fc&=&width=756&height=534' ,date: "2023-10-06", review: "Good game but has bugs.", rating: 3 }
   ];
 
   const handleSubmit = () => {
