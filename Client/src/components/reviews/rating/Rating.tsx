a9e298243fc598841080a958ca035806e7209769
Hasan Safdari <41860385+tahasafdari@users.noreply.github.com>
2023-10-10 19:28:00 +0300

    Add star rating for users to give star to a game

--- Client/src/components/reviews/rating/Rating.tsx
+++ Client/src/components/reviews/rating/Rating.tsx
@@ -1,23 +1,26 @@
-// components/Rating.tsx
-
 import React from 'react';
 import ReactStars from 'react-stars';
 
 type Props = {
+  value?: number;  // <- Add this prop to set a fixed value
   maxRating?: number,
-  onRatingChange: (rating: number) => void,
+  onRatingChange?: (rating: number) => void,
+  isEditable?: boolean
 };
 
-const Rating = ({ maxRating = 5, onRatingChange } : Props) => {
+const Rating = ({ value, maxRating = 5, onRatingChange, isEditable = true } : Props) => {
 
   return (
     <ReactStars
+      value={value}  // <- Use the value prop here
       count={maxRating}
-      onChange={onRatingChange}
+      onChange={onRatingChange || (() => {})} // <- Prevent errors when onRatingChange is not provided
       size={24}
       color1={'gray'}
       color2={'#ffd700'}
       half={true} 
+     // edit={onRatingChange ? true : false} // <- Allow editing only if the onRatingChange function is provided
+      edit={isEditable}
     />
   );
 };
